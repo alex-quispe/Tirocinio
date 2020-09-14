fdf0926d50ca165821f440567c4a0a587da9967c
Alex Quispe <68338273+alex-quispe@users.noreply.github.com>
2020-09-14 11:56:53 +0200

    Add files via upload
    aggiunto nuove funzioni

--- Main.py
+++ Main.py
@@ -8,7 +8,14 @@ from noise import img_noise
 from sharpness import img_sharpness
 
 from white import img_white
+
+
 from rotazione import img_rotazione
+from traslazione import img_traslata
+
+from Flare import img_flare
+from ColorTemperature import img_colorTemperature
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -23,33 +30,56 @@ if __name__ == '__main__':
     group.add_argument('--sharpness', action='store_true',help="nitidezza dell'immagine ")
     #group.add_argument('--sovrapposizine', action='store_true',help="immagine sovrapposto")
     group.add_argument('--white', action='store_true',help="immagine completamente biacno")
+    
     group.add_argument('--rotazione', action='store_true',help="immagine ruotato")
-   
+    group.add_argument('--traslazione', action='store_true',help="immagine traslato")
+    group.add_argument('--flare', action='store_true',help="immagine con flare")
+    
+    
+    
+    group.add_argument('--temperatura', action='store_true',help="immagine traslato")
+    
     group.add_argument('--all', action='store_true',help="tutte le elavorazioni")
-    parser.add_argument('--value', type=float, default=0, help="valore per alcune elavorazioni")
+    
+    group1 = parser.add_mutually_exclusive_group()
+    
+    group1.add_argument('--value1', type=float, default=0, help="valore per alcune elavorazioni")
+    group1.add_argument('--immagine1', help="immagine da elavorare" )
+    
+    parser.add_argument('--value2', type=float, default=0, help="valore per alcune elavorazioni")
     args = parser.parse_args()  
     
     if args.black:
         img_black(args.immagine)
     elif args.blurred:
-        img_blurred(args.immagine,args.value)
+        img_blurred(args.immagine,args.value1)
     elif args.brightness:
-        img_brightness(args.immagine,args.value)
+        img_brightness(args.immagine,args.value1)
     elif args.chromatic:
-        img_chromatic(args.immagine,args.value)
+        img_chromatic(args.immagine,args.value1)
     elif args.greyscale:
         img_greyscale(args.immagine)
     elif args.noise:
         img_noise(args.immagine)
     elif args.sharpness:
-        img_sharpness(args.immagine, args.value)
+        img_sharpness(args.immagine, args.value1)
         
     
     elif args.white:
         img_white(args.immagine)
-    elif args.rotazione:
-        img_rotazione(args.immagine, args.value)
     
+    
+    
+    elif args.rotazione:
+        img_rotazione(args.immagine, args.value1)
+    elif args.traslazione:
+        img_traslata(args.immagine, args.value1,args.value2)
+        
+    
+    elif args.flare:
+        img_flare(args.immagine, args.immagine1)
+    elif args.temperatura:
+        img_colorTemperature(args.immagine, args.value1)
     
     
     
