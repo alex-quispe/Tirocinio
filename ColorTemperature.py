70ecc4797f740f4902e0d419110775a55d0b88f6
Alex Quispe <68338273+alex-quispe@users.noreply.github.com>
2020-09-14 11:57:38 +0200

    Add files via upload

---
+++ ColorTemperature.py
@@ -0,0 +1,80 @@
+from matplotlib import pyplot as plt
+from PIL import Image
+import math
+
+def convert_K_to_RGB(colour_temperature):
+    """
+    Converts from K to RGB, algorithm courtesy of 
+    http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
+    """
+    #range check
+    if colour_temperature < 1000: 
+        colour_temperature = 1000
+    elif colour_temperature > 40000:
+        colour_temperature = 40000
+    
+    tmp_internal = colour_temperature / 100.0
+    
+    # red 
+    if tmp_internal <= 66:
+        red = 255
+    else:
+        tmp_red = 329.698727446 * math.pow(tmp_internal - 60, -0.1332047592)
+        if tmp_red < 0:
+            red = 0
+        elif tmp_red > 255:
+            red = 255
+        else:
+            red = tmp_red
+    
+    # green
+    if tmp_internal <=66:
+        tmp_green = 99.4708025861 * math.log(tmp_internal) - 161.1195681661
+        if tmp_green < 0:
+            green = 0
+        elif tmp_green > 255:
+            green = 255
+        else:
+            green = tmp_green
+    else:
+        tmp_green = 288.1221695283 * math.pow(tmp_internal - 60, -0.0755148492)
+        if tmp_green < 0:
+            green = 0
+        elif tmp_green > 255:
+            green = 255
+        else:
+            green = tmp_green
+    
+    # blue
+    if tmp_internal >=66:
+        blue = 255
+    elif tmp_internal <= 19:
+        blue = 0
+    else:
+        tmp_blue = 138.5177312231 * math.log(tmp_internal - 10) - 305.0447927307
+        if tmp_blue < 0:
+            blue = 0
+        elif tmp_blue > 255:
+            blue = 255
+        else:
+            blue = tmp_blue
+    
+    return red, green, blue
+
+
+def img_colorTemperature(immagine,value):
+    picture1=Image.open((immagine))
+    picture = Image.open(immagine)
+    
+    r,g,b=convert_K_to_RGB(value)
+    color_matrix = (r / 255.0, 0.0, 0.0, 0.0, 0.0, g / 255.0, 0.0, 0.0, 0.0, 0.0, b / 255.0, 0.0)  
+    picture=picture.convert('RGB',color_matrix)     
+    
+    plt.figure(num='Fallimento Color Temperature')
+    plt.subplot(121),plt.imshow(picture1),plt.title('Originale')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(picture),plt.title('Color Temperature')
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
